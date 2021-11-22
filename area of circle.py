f7147108cf35146c6c0c4291423999c8ea11faf6
AnaghaPNair03 <93936223+AnaghaPNair03@users.noreply.github.com>
2021-11-22 19:08:44 +0530

    Add files via upload

---
+++ area of circle.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov 13 19:23:32 2021
+
+@author: Anagha
+"""
+
+# Write a python program which accepts the radius of a circle from 
+# the user and computes area.
+
+r=1.1
+pi=3.1415
+a=pi*(r)**2
+print(a)
---
+++ parameters.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 22 17:09:16 2021
+
+@author: Anagha
+"""
+
+def zero(x,y):
+    print(x+y)
+    
+      
+def add(a,b,c):
+    print(a+b+c)
+
+add(2,3,5) 
+
+def addition(d,e,f,g):
+    print(d+e+f+g)
+    
+addition(6,4,2,7)    
+
+
+    
+          
+          
+    
+    
